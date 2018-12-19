958efbcf4f97550e779d5ce79c08157c38e5925f
ZhongTongZhuo <zhongtongzhuo@126.com>
2018-12-19 15:12:22 +0800

    delete recurrence

--- User.c
+++ User.c
@@ -3,8 +3,14 @@
 //
 
 #include "user.h"
+/*
+#include "fileProcessing.h"
+#ifdef __clang__
+	#include "fileProcessing.c"
+
+#endif
+*/
 int A=0;
-//文件准备函数
 void compatiablyFlush()
 {
 	#ifdef _MSC_VER
@@ -13,6 +19,8 @@ void compatiablyFlush()
 		fflush(stdin);
 	#endif
 }
+
+//文件准备函数
 void aboutFile()
 {
     FILE *fp = fopen("test.txt", "r");
@@ -40,51 +48,53 @@ void date()
 //主界面函数
 void Main()
 {
-    char i;
-    system("cls");
-    aboutFile();
-    system("color 31");
-    date();
-    printf("\n\n\n\t\t\tPlease select your identity category: ");
-    printf("\n\n\t\t\t1.user   ");
-    printf("\n\n\t\t\t2.admin   ");
-    printf("\n\n\t\t\t3.exit   ");
-    printf("\n\n\n");
-    printf("\t\t\tplease choose:");
-    ////compatiablyFlush();
-	i = getchar();//读走换行符
-	getchar();
-    if(i=='1')
-        userInterface();
-    if(i=='2')
-        adminInterface();
-	if (i == '3')
-		exit(0);
+	do
+	{
+		char i;
+		system("cls");
+		aboutFile();
+		system("color 31");
+		date();
+		printf("\n\n\n\t\t\tPlease select your identity category: ");
+		printf("\n\n\t\t\t1.user   ");
+		printf("\n\n\t\t\t2.admin   ");
+		printf("\n\n\t\t\t3.exit   ");
+		printf("\n\n\n");
+		printf("\t\t\tplease choose:");
+		i = getchar();
+		getchar();//读走换行符
+		if (i == '1') userInterface();
+		else if (i == '2') adminInterface();
+		else if (i == '3') break;
+		else printf("Invalid input!");
+	} while (1);
 }
 
 //用户界面
 void userInterface()
 {
-    system("cls");
-    date();
-    char i;
-    FILE *fp = fopen("test.txt", "r");
-    if (!fp)
-        printf("fail to read file\n");
-    printf("\n\n\n\n\t\t\t*******please choose your entrance*******");
-    printf("\n\n\t\t\t1.log on\n\n\t\t\t2.sign up\n\n\t\t\t3.report for lost\n\n\t\t\t4.exit\n");
-    printf("\n\n\t\t\tplease input your choice:");
-	//compatiablyFlush();
-	i = getchar();
-	getchar();//读走换行符
-    if(i=='1')
-        logIn();
-    if(i=='2')
-        signIn();
-    if(i=='3')
-        missAccount();
-    if(i=='4')
-        Main();           //回到主界面
+	do
+	{
+		system("cls");
+		date();
+		char i;
+		FILE *fp = fopen("test.txt", "r");
+		if (!fp)
+			printf("fail to read file\n");
+		printf("\n\n\n\n\t\t\t*******please choose your entrance*******");
+		printf("\n\n\t\t\t1.log on\n\n\t\t\t2.sign up\n\n\t\t\t3.report for lost\n\n\t\t\t4.exit\n");
+		printf("\n\n\t\t\tplease input your choice:");
+		//compatiablyFlush();
+		i = getchar();
+		getchar();//读走换行符
+		switch (i)
+		{
+			case'1':logIn(); break;
+			case'2':signIn(); break;
+			case'3':missAccount(); break;
+			case'4':return;//回到主界面
+		}
+	} while (1);
 }
 
 void logIn()
@@ -113,7 +123,7 @@ void logIn()
             {
                 printf("\n\n\t\tthis card is reported missing");
                 getchar();
-                userInterface();
+				return;
             }
             else
                 break;
@@ -123,7 +133,7 @@ void logIn()
     {
         printf("\n\n\t\tthis account doesn't exist or being signed off, \n\t\tplease sign up and log on (input Enter to return)");
         getchar();
-        userInterface();
+		return;
     }
     //登录密码次数的现在
     while(chanceNumber--)
@@ -146,6 +156,7 @@ void logIn()
     printf("\n\n\t\tinput any key to continue->");
     getchar();
     userOperate(i);
+	return;
 }
 
 void signIn()
@@ -171,7 +182,7 @@ void signIn()
     {
         printf("\n\t\tthis account has been signed up, try again please!(input Enter to return)");
         getchar();
-        userInterface();
+		return;
     }
     strcpy(user[A].accounts, inputAccounts);
     printf("\n\t\tplease input your password(less than 6 letter):");
@@ -186,7 +197,7 @@ void signIn()
     fclose(op);
     printf("\n\n\t\treturn to last page, input random key to continue->");
     getchar();
-    userInterface();                //返回到主界面
+    return;               //返回到主界面
 }
 
 void missAccount()
@@ -225,36 +236,37 @@ void missAccount()
     fclose(fz);
     printf("\n\n\t\treturn to last page, input random key to continue->");
     getchar();
-    userInterface();
+	return;
 }
 
 void userOperate(int n)
 {
-    system("cls");
-    date();
-    char i;
-    printf("\n\n\n\n\t\t\t*******choose your wanted *******");
-    printf("\n\n\t\t\t1.save money\n\n\t\t\t2.withdraw money  \n\n\t\t\t3.change password  \n\n\t\t\t4.user sign off  \n\n\t\t\t5.search info \n\n\t\t\t6.exit \n");
-    printf("\n\t\tplease input your choice:");
-    //compatiablyFlush();
-	i = getchar();
-	getchar();
-    if(i=='1')
-        deposit(n);
-    if(i=='2')
-        withdraw(n);
-    if(i=='3')
-        changePassword(n);
-    if(i=='4')
-        lostUser(n);
-    if(i=='5')
-        showInfo(n);
-    if(i=='6')
-        userInterface();
+	do
+	{
+		system("cls");
+		date();
+		char i;
+		printf("\n\n\n\n\t\t\t*******choose your wanted *******");
+		printf("\n\n\t\t\t1.save money\n\n\t\t\t2.withdraw money  \n\n\t\t\t3.change password  \n\n\t\t\t4.user sign off  \n\n\t\t\t5.search info \n\n\t\t\t6.exit \n");
+		printf("\n\t\tplease input your choice:");
+		//compatiablyFlush();
+		i = getchar();
+		getchar();
+		switch (i)
+		{
+			case'1':deposit(n); break;
+			case'2':withdraw(n); break;
+			case'3':changePassword(n); return;//修改密码后要求重新登陆
+			case'4':lostUser(n); break;
+			case'5':showInfo(n); break;
+			case'6':return;
+		}
+	} while (1);
 }
 
 void deposit(int n)
 {
+	system("cls");
     date();
     int money;
     double TS;               //暂存数据;
@@ -276,11 +288,12 @@ void deposit(int n)
     fclose(fz);
     printf("\n\n\t\treturn to last page, input random key to continue->");
     getchar();
-    userOperate(n);
+	return;
 }
 
 void withdraw(int n)
 {
+	system("cls");
     date();
     int  money;
     double TS;
@@ -302,7 +315,7 @@ void withdraw(int n)
     fclose(fp);
     printf("\n\n\t\treturn to last page, input random key to continue->");
     getchar();
-    userOperate(n);
+	return;
 }
 
 void changePassword(int n)
@@ -340,7 +353,7 @@ void changePassword(int n)
     printf("\n\t\tchange password successful!");
     printf("\n\n\t\treturn to main page,input Enter to continue->");
     getchar();
-    userInterface();
+	return;
 }
 
 void lostUser(int n)
@@ -382,7 +395,7 @@ void lostUser(int n)
     printf("\n\t\tuser has been signed off\n");
     printf("\n\n\t\treturn to main page,input Enter to continue->");
     getchar();
-    userInterface();
+ 
 }
 
 void showInfo(int n)
@@ -393,7 +406,7 @@ void showInfo(int n)
     printf("\n\t\tbalance:%.2lf\n", user[n].prestore);
     printf("\n\n\t\treturn to last page, input random key to continue->");
     getchar();
-    userOperate(n);
+	return;
 }
 
 void adminInterface()
@@ -430,25 +443,28 @@ void adminInterface()
 
 void adminOperate()
 {
-    system("cls");
-    date();
-    char i;
-    FILE *fp = fopen("test.txt", "r");
-    if (!fp)
-        printf("fail to read file\n");
-    fclose(fp);
-    printf("\n\n\n\n\n\n\t\t\t*****please choose your entrance*****  ");
-    printf("\n\n\t\t\t1.confirm users' info  \n\n\t\t\t2.confirm reported missing users  \n\n\t\t\t3.exit     \n\n");
-    printf("\t\t\tinput your choice:");
-    //compatiablyFlush();
-	i = getchar();
-	getchar();
-    if(i=='1')
-        search();
-    if(i=='2')
-        isLost();
-    if(i=='3')
-        Main();
+	do
+	{
+		system("cls");
+		date();
+		char i;
+		FILE *fp = fopen("test.txt", "r");
+		if (!fp)
+			printf("fail to read file\n");
+		fclose(fp);
+		printf("\n\n\n\n\n\n\t\t\t*****please choose your entrance*****  ");
+		printf("\n\n\t\t\t1.confirm users' info  \n\n\t\t\t2.confirm reported missing users  \n\n\t\t\t3.exit     \n\n");
+		printf("\t\t\tinput your choice:");
+		//compatiablyFlush();
+		i = getchar();
+		getchar();
+		switch (i)
+		{
+			case'1':search(); break;
+			case'2':isLost(); break;
+			case'3':return;
+		}
+	} while (1);
 }
 
 void search()
@@ -466,7 +482,7 @@ void search()
                user[i].accounts, user[i].password, user[i].name,user[i].prestore,user[i].isMissing);
     printf("\n\n\t\tfinished, input random key to return");
     getchar();
-    adminOperate();
+	return;
 }
 
 void isLost()
@@ -486,7 +502,5 @@ void isLost()
     }
     printf("\n\n\t\t\tfinished, input random key to return");
     getchar();
-    adminOperate();
+	return;
 }
-
-
